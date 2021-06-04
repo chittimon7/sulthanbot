76a029069136ba1dae44bb9f750f28663c5cb7bb
chittimon7 <85331951+chittimon7@users.noreply.github.com>
2021-06-04 12:09:51 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption,
+                caption = @srsuggestionsmc,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🛡️Join Our Channel🛡️', url="https://t.me/srsuggestionsmc"
                                 )
                         ]
                     ]
